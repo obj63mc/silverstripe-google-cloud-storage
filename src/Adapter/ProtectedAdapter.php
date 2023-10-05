d26ee523a9ccde31dc1426994e92f56f3c819ec5
Rick Kuipers <rick@wedevelop.nl>
2023-10-05 13:04:19 +0200

    Implement new adapter

--- src/Adapter/ProtectedAdapter.php
+++ src/Adapter/ProtectedAdapter.php
@@ -2,27 +2,22 @@
 
 namespace SilverStripe\GoogleCloudStorage\Adapter;
 
-use Google\Cloud\Storage\StorageClient;
-use InvalidArgumentException;
-use Superbalist\Flysystem\GoogleStorage\GoogleStorageAdapter;
+use League\Flysystem\GoogleCloudStorage\GoogleCloudStorageAdapter;
 use SilverStripe\Assets\Flysystem\ProtectedAdapter as SilverstripeProtectedAdapter;
 
-/**
- * An adapter that allows the use of Google Cloud Storage to store and transmit assets rather than storing them locally.
- */
-class ProtectedAdapter extends GoogleStorageAdapter implements SilverstripeProtectedAdapter
+class ProtectedAdapter extends GoogleCloudStorageAdapter implements SilverstripeProtectedAdapter
 {
+    private BucketAdapter $bucketAdapter;
 
-
-    public function __construct(BucketAdapter $bucketAdapter, $prefix = null, $storageApiUri = null)
+    public function __construct(BucketAdapter $bucketAdapter, string $prefix = null)
     {
-        if (!$bucketAdapter) {
-            throw new InvalidArgumentException("GC_BUCKET_NAME environment variable not set");
-        }
         if (!$prefix) {
             $prefix = 'protected';
         }
-        parent::__construct($bucketAdapter->getClient(), $bucketAdapter->getBucket(), $prefix, $storageApiUri);
+
+        $this->bucketAdapter = $bucketAdapter;
+
+        parent::__construct($bucketAdapter->getBucket(), $prefix);
     }
 
 
@@ -31,17 +26,15 @@ class ProtectedAdapter extends GoogleStorageAdapter implements SilverstripeProte
      *
      * @return string
      */
-    public function getProtectedUrl($path)
+    public function getProtectedUrl($path): string
     {
-        $object = $this->getObject($path);
-        $signedUrl = $object->signedUrl(date_create('+1 day'), []);
-        if ($this->getStorageApiUri() !== self::STORAGE_API_URI_DEFAULT) {
-            if (!isset($options['cname'])) {
-                list($url, $params) = explode('?', $signedUrl, 2);
-                $signedUrl = $this->getUrl($path) . '?' . $params;
-            }
-        }
+        $object = $this->bucketAdapter->getBucket()->object($path);
+        return $object->signedUrl(date_create('+1 day'));
+    }
 
-        return $signedUrl;
+    public function getVisibility($path)
+    {
+        // Save an API call
+        return ['path' => $path, 'visibility' => self::VISIBILITY_PRIVATE];
     }
 }
