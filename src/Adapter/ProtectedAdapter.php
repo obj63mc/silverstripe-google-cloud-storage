c5ef8291608e05b5dc4191e1442ec75cfe0bfd80
Rick Kuipers <rick@wedevelop.nl>
2023-11-08 19:16:10 +0100

    Rely on temporaryUrl to ensure use of prefixer

--- src/Adapter/ProtectedAdapter.php
+++ src/Adapter/ProtectedAdapter.php
@@ -2,20 +2,17 @@
 
 namespace SilverStripe\GoogleCloudStorage\Adapter;
 
+use League\Flysystem\Config;
 use League\Flysystem\GoogleCloudStorage\GoogleCloudStorageAdapter;
 use SilverStripe\Assets\Flysystem\ProtectedAdapter as SilverstripeProtectedAdapter;
 
 class ProtectedAdapter extends GoogleCloudStorageAdapter implements SilverstripeProtectedAdapter
 {
-    private BucketAdapter $bucketAdapter;
-
     public function __construct(BucketAdapter $bucketAdapter, string $prefix = null)
     {
         if (!$prefix) {
             $prefix = 'protected';
         }
-
-        $this->bucketAdapter = $bucketAdapter;
 
         parent::__construct($bucketAdapter->getBucket(), $prefix);
     }
@@ -28,13 +25,6 @@ class ProtectedAdapter extends GoogleCloudStorageAdapter implements Silverstripe
      */
     public function getProtectedUrl($path): string
     {
-        $object = $this->bucketAdapter->getBucket()->object($path);
-        return $object->signedUrl(date_create('+1 day'));
-    }
-
-    public function getVisibility($path)
-    {
-        // Save an API call
-        return ['path' => $path, 'visibility' => self::VISIBILITY_PRIVATE];
+        return $this->temporaryUrl($path, new \DateTime('+1 day'), new Config());
     }
 }
